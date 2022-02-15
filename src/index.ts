1b0ce3a130ce31bad6ab9e79864c55157c673bb6
Sebastian <sebastian.cornejo@walmart.com>
2022-02-15 19:57:53 -0300

    imprubment dev env

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // TODO: Se debe obtener la informacion del calendario de vacunas
+// desde la url https://www.minsal.cl/calendario-de-vacunacion-masiva-contra-covid-19/ obtener la informacion de la vacuna
 console.log('Obteniendo informacion de calendario de vacunas');
 
 // TODO: Si encuentra informacion nueva se debe comunicar por correo o whasapp, si no encuentra informacion nueva no debe hacer nada
