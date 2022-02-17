7e78f4b62316b485f354b0f8df1854ab003d244d
Sebastian <sebastian.cornejo@walmart.com>
2022-02-16 21:53:56 -0300

    add create folder if not exist - images

--- src/getData.ts
+++ src/getData.ts
@@ -8,6 +8,8 @@ export default async function GetData() {
 
 	const URL_VACUNATION_CALENDAR: string = 'http://www.corporacionbuin.cl';
 	try {
+		await mkdirpSync('src/images');
+
 		let res: AxiosResponse = await axios.get(URL_VACUNATION_CALENDAR);
 		const html = await res.data;
 		const $ = cheerio.load(html);
@@ -17,14 +19,14 @@ export default async function GetData() {
 
 		images.each((i, elem) => {
 			const urlImange = $(elem).attr('src');
-			if (urlImange) urlToObject(urlImange);
+			if (urlImange) urlToObject(urlImange, 'images');
 		});
 	} catch (err) {
-		console.log('Error al obtener informacion de calendario de vacunas');
+		console.log('Error al obtener informacion de calendario de vacunas: ', err);
 	}
 }
 
-const urlToObject = async (fileUrl: string, downloadFolder = 'images') => {
+const urlToObject = async (fileUrl: string, downloadFolder: string) => {
 	const fileName = path.basename(fileUrl);
 	try {
 		const localFilePath = path.resolve(__dirname, downloadFolder, fileName);
@@ -39,7 +41,22 @@ const urlToObject = async (fileUrl: string, downloadFolder = 'images') => {
 		w.on('finish', () => {
 			console.log('Successfully downloaded file!');
 		});
+		w.on('error', () => {
+			console.log('fail!');
+		});
 	} catch (err) {
 		console.log('Error to get image from url: ', err);
+	}
+};
+
+var mkdirpSync = function (dirpath: string) {
+	var parts = dirpath.split(path.sep);
+	for (var i = 1; i <= parts.length; i++) {
+		try {
+			fs.mkdirSync(path.join.apply(null, parts.slice(0, i)));
+		} catch (e: any) {
+			if (e.code != 'EEXIST') throw e;
+			else console.log('Folder already exists');
+		}
 	}
 };
